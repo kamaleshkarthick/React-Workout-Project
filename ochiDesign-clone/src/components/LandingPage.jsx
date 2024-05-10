45ec0f33d540721f427273d8017fd970d1ce02cb
Kamalesh Karthick <76035160+kamaleshkarthick@users.noreply.github.com>
2024-05-10 19:38:02 +0530

    Landing page

--- ochiDesign-clone/src/components/LandingPage.jsx
+++ ochiDesign-clone/src/components/LandingPage.jsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-
 const LandingPage = () => {
   return (
     <>
-      <div className="w-full h-screen bg-zinc-900 pt-1">
-        <div className="textstructure mt-40 px-20">
-                  <div className="masker">
-                      <div className=" uppercase text-9xl leading-none  font-FoundersGrotesk "> We Create</div>
-          </div>
+      <div className="w-full h-screen bg-zinc-900 ">
+        <div className="textstructure pt-72 pb-80 px-20">
+          {["We create", "Eye-Opening", "Presentations"].map((item, i) => {
+            return (
+              <div className="masker" key={i}>
+                <h1 className=" uppercase text-9xl leading-[.9] font-FoundersGrotesk  font-semibold">
+                  {item}
+                </h1>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
